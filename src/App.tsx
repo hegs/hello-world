be95d8d5d950cef913534bd05e0dbcd92621b400
hegs <davidhegarty@gmail.com>
2025-07-21 18:51:40 -0700

    [Magic Patterns] Updated from 'Basic Hello World App Creation'

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export function App() {
+  return <div className="flex w-full min-h-screen justify-center items-center bg-gradient-to-r from-blue-50 to-indigo-50">
+      <h1 className="text-4xl font-bold text-gray-800 animate-pulse">
+        Hello World!
+      </h1>
+    </div>;
+}
